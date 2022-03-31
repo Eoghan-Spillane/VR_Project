7169841f02a44545d2dbf475adaefa3c50bdd255
Eoghan Spillane <eoghan.spillane@mycit.ie>
2022-03-31 14:24:34 +0100

    Added Dynamic Batching

--- Assets/Scripts/GridSpawner.cs
+++ Assets/Scripts/GridSpawner.cs
@@ -13,6 +13,7 @@ public class GridSpawner : MonoBehaviour
     private int objectCount;
 
     void Update(){
+        // Count the objects
         time += Time.deltaTime;
 
         // If it's been 1 second, update total object text count
@@ -22,26 +23,36 @@ public class GridSpawner : MonoBehaviour
         }
     }
 
-    
-
     public GameObject[] objectsToSpawn;
+
+    private List<GameObject> spawnedObjects = new List<GameObject>();
     public int gridX;
     public int gridZ;
     public float gridSpacingOffset = 1f;
     public Vector3 gridOrigin = Vector3.zero;
     public void spawnGrid(){
-
         for (int x = 0; x < gridX; x++){
             for(int z = 0; z < gridZ; z++){
                 Vector3 spawnPosition = new Vector3(x * gridSpacingOffset, 0, z * gridSpacingOffset) + gridOrigin;
-                pickAndSpawn(spawnPosition, Quaternion.identity);
+                spawnObject(spawnPosition, Quaternion.identity);
             }
-        }    
+        }
     }
 
-    void pickAndSpawn(Vector3 positionToSpawn, Quaternion rotationToSpawn){
+    void spawnObject(Vector3 positionToSpawn, Quaternion rotationToSpawn){
         int randomIndex = Random.Range(0, objectsToSpawn.Length);
+        
         GameObject clone = Instantiate(objectsToSpawn[randomIndex], positionToSpawn, rotationToSpawn);
         objectCount++;
+
+        spawnedObjects.Add(clone);
+    }
+
+    public void despawn(){
+       foreach(var objectete in spawnedObjects){
+           Destroy(objectete);
+       } 
+
+       objectCount = 0;
     }
 }
